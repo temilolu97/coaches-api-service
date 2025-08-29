1338346572da6e2a51cef170e6bb25030e19cb04
temilolu97 <temiloluwaabiodunojo@gmail.com>
2025-08-29 14:08:01 +0100

    updates

--- src/app.js
+++ src/app.js
@@ -7,5 +7,8 @@ import authRoutes from './routes/authRoutes.js'
 import paymentRoutes from './routes/paymentRoutes.js'
 app.use('/api/auth', authRoutes)
 app.use('/api/payments',paymentRoutes)
+app.get("/", (req,res)=>{
+    res.json({message:"Welcome to Coaches API Service"})
+})
 export default app
 
